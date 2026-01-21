1bf70a1357d5dda6118b96b1476030fc4e773c24
Ted Dorfeuille <teddorfeuille@gmail.com>
2026-01-21 08:58:38 -0600

    init

---
+++ .config/nvim.hold/init.lua
@@ -0,0 +1,5 @@
+-- KEYS
+vim.g.mapleader = ","
+
+-- LAZY
+require("config.lazy")
---
+++ .config/nvim.hold/lua/plugins/init.lua
@@ -0,0 +1,15 @@
+return {
+	-- PLUGINS
+		{
+		"vim-airline/vim-airline",
+		"tpope/vim-fugitive",
+		"tpope/vim-commentary",
+		
+		-- https://github.com/nvim-telescope/telescope.nvim
+		{
+		    'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
+		     dependencies = { 'nvim-lua/plenary.nvim' }
+		},
+	},
+
+}
