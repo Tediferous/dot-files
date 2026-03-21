f75a1317fc475383054c8d3fa411aee7d8306355
Ted Dorfeuille <teddorfeuille@gmail.com>
2026-03-21 15:08:30 -0500

    vim

---
+++ vim/.config/nvim/lua/tediferouslight/palette.lua
@@ -0,0 +1,70 @@
+return {
+  -- full gradient
+  g00 = "#000000",
+  g01 = "#08383C",
+  g02 = "#184D4F",
+  g03 = "#096461",
+  g04 = "#0D7368",
+  g05 = "#4D804B",
+  g06 = "#A57D17",
+  g07 = "#C49B47",
+  g08 = "#E3B976",
+  g09 = "#F5DCC4",
+  g10 = "#FFFFFF",
+
+  -- colors
+  red = "#C72D4D",
+  orange = "#0D7368",
+  green = "#4D804B",
+  yellow = "#0D7368",
+  blue = "#096461",
+  magenta = "#184D4F",
+  cyan = "#08383C",
+  black = "#FFFFFF",
+  bright_red = "#C72D4D",
+  bright_orange = "#0D7368",
+  bright_green = "#4D804B",
+  bright_yellow = "#0D7368",
+  bright_blue = "#096461",
+  bright_magenta = "#184D4F",
+  bright_cyan = "#08383C",
+  white = "#000000",
+  gutter_fg = "#08383C",
+  nontext = "#F5DCC4",
+
+  -- element approach
+
+  bg = "#F5DCC4",
+  fg = "#08383C",
+  cursorline = "#08383C",
+  selection = "#C49B47",
+  linenr = "#A57D17",
+  comment = "#A57D17",
+
+  -- Syntax
+  keyword = "#4D804B",
+  Function = "#C72D4D",
+  string = "#A57D17",
+  constant = "#4D804B",
+  type = "#4D804B",
+  number = "#4D804B",
+  boolean = "#C72D4D",
+  operator = "#C72D4D",
+  variable = "#4D804B",
+
+  -- UI
+  cursor = "#000000",
+  visual = "#08383C",
+  search = "#08383C",
+  statusline = "#08383C",
+  menu_bg = "#E3B976",
+  menu_sel = "#C49B47",
+  fold = "#E3B976",
+  split = "#08383C",
+
+  -- Diagnostics
+  diag_error = "#C72D4D",
+  diag_warn = "#C49B47",
+  diag_info = "#000000",
+  diag_hint = "#4D804B",
+}
