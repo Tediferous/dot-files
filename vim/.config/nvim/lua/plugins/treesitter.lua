a69901e9e5a5c46edf6f99ea981bbd6fcde4096a
Ted Dorfeuille <teddorfeuille@gmail.com>
2026-04-11 17:50:01 -0500

    tree

---
+++ vim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,63 @@
+return {
+  -- Tree sitter based syntax highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function(_, opts)
+      local ensureInstalled = {
+        "bash",
+        "comment",
+        "css",
+        "diff",
+        "fish",
+        "gitcommit",
+        "git_rebase",
+        "graphql",
+        "go",
+        "html",
+        "ini",
+        "javascript",
+        "just",
+        "json",
+        "lua",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "regex",
+        "rust",
+        "svelte",
+        "templ",
+        "terraform",
+        "toml",
+        "tsx",
+        "typescript",
+        "vimdoc",
+        "yaml",
+      }
+      local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+      local parsersToInstall = vim
+        .iter(ensureInstalled)
+        :filter(function(parser)
+          return not vim.tbl_contains(alreadyInstalled, parser)
+        end)
+        :totable()
+      require("nvim-treesitter").install(parsersToInstall)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        desc = "User: enable treesitter highlighting",
+        callback = function(ctx)
+          -- highlights
+          local hasStarted = pcall(vim.treesitter.start) -- errors for filetypes with no parser
+
+          -- indent
+          local noIndent = {}
+          if hasStarted and not vim.list_contains(noIndent, ctx.match) then
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+    end,
+  },
+}
