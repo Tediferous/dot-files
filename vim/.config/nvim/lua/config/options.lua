2f9dec344ea0bd8516964f76d48e54d636cbe630
Ted Dorfeuille <teddorfeuille@gmail.com>
2026-04-25 15:20:29 -0500

    telescope, golang, no tabs

--- vim/.config/nvim/lua/config/options.lua
+++ vim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.mapleader = ","
+vim.opt.mouse = ""
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
