869dc60555c389eebe1f0d7de68d3eab61185920
Ted Dorfeuille <teddorfeuille@gmail.com>
2026-05-16 15:23:44 -0500

    only notes

--- vim/.config/nvim/lua/plugins/wiki.lua
+++ vim/.config/nvim/lua/plugins/wiki.lua
@@ -1,8 +1,9 @@
-{
-  'vimwiki/vimwiki',
+return {
+  "vimwiki/vimwiki",
   init = function()
-    vim.g.vimwiki_path = '~/notes/'
-    vim.g.vimwiki_syntax = 'markdown'
-    vim.g.vimwiki_ext = 'md'
+    vim.g.vimwiki_path = "~/notes/"
+    vim.g.vimwiki_syntax = "markdown"
+    vim.g.vimwiki_ext = "md"
+    vim.g.vimwiki_global_ext = 0
   end,
 }
